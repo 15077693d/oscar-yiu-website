7638e0ce51e7c01a662ad570f6dea341e36d5a8d
15077693d <15077693d@gmail.com>
2020-12-21 00:58:50 +0800

    correct form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -18,37 +18,16 @@ const Contact = () => {
         }
     }))
     const classes = useStyle()
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [content, setContent] = useState("")
-    const handleSubmit = (e) => {
-        function encode(data) {
-            return Object.keys(data)
-                .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-                .join("&")
-        }
-
-        e.preventDefault()
-        fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({
-                "form-name": e.target.getAttribute("name"),
-                "name": name, "email": email, "content": content
-            })
-        }).then(() => console.log("Thank!")).catch(error => alert(error))
-
-    }
     return (
         <Layout>
             <TitleSubtitle title={"聯絡我們"} subtitle={"你的留言會電郵到我的電子郵箱，我們會在一至兩日內回覆。"} tags={[]} />
-            <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+            <form name="contact" method="POST" data-netlify="true">
                 <input type="hidden" name="form-name" value="contact" />
                 <Grid container direction="column" alignItems="center">
                     <Box className={classes.container}>
                         <Grid container direction="row" justify="space-between">
-                            <TextField name="name" label="稱呼" variant="outlined" style={{ width: "45%" }} onChange={e => setName(e.target.value)} value={name} required />
-                            <TextField name="email" type="email" label="電郵地址" variant="outlined" style={{ width: "50%" }} onChange={e => setEmail(e.target.value)} value={email} required />
+                            <TextField name="name" label="稱呼" variant="outlined" style={{ width: "45%" }}  required />
+                            <TextField name="email" type="email" label="電郵地址" variant="outlined" style={{ width: "50%" }}  required />
                         </Grid>
                         <TextField name="content" multiline={true} style={{ marginTop: marginY, width: "100%" }}
                             inputProps={{
@@ -56,7 +35,7 @@ const Contact = () => {
                                     height: "20vh",
                                     width: "100%"
                                 },
-                            }} label="你的留言" variant="outlined" onChange={e => setContent(e.target.value)} value={content} required />
+                            }} label="你的留言" variant="outlined" required />
                     </Box>
                     <Btn text="確定" isSubmit={true} />
                 </Grid>
